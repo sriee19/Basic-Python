34770b90e3bfdad514c21909fd400af7213f3697
SrisanjanaA <88726874+sriee19@users.noreply.github.com>
2023-04-25 23:58:03 +0530

    Add files via upload

---
+++ Getting Input.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Getting Input
+
+# instead of print we use input to get inputs from the users
+
+# In[1]:
+
+
+name = input('What is your name? ')
+print('Hi! ' + name)
+
+
+# here we collected the name of the person and printed with hi!
+
+# # Question
+
+# Ask two Questions: person's name and favorite color.
+# then, print a message like "San like red"
+
+# In[3]:
+
+
+getname = input('whats your name?  ')
+getcolor = input('whats your fav color? ')
+print(getname + ' LIKES ' + getcolor)
+
+
+# In[ ]:
+
+
+
+
