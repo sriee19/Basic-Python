27c0b4993656c78e864b2e171f32b9df479c7fab
SrisanjanaA <88726874+sriee19@users.noreply.github.com>
2023-04-26 00:37:31 +0530

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Strings
+
+# single or double qoutes can be used for defining a string
+
+# In[2]:
+
+
+Course = "Python's course for Begginers"
+print(Course)
+
+
+# to define multiple lines use triple quotes 
+
+# In[3]:
+
+
+print(Course[3])
+
+
+# the above one is the index defining the characters
+
+# using negative index we count the character from the end
+
+# In[4]:
+
+
+print(Course[-1])
+
+
+# to extract multiple characters
+
+# In[5]:
+
+
+print(Course[0:4])
+
+
+# from 0 index to 4th excluding the 4th
+
+# In[6]:
+
+
+another = Course[:]
+print(another)
+
+
+# # What would be the output of the given terminal
+
+# In[9]:
+
+
+name = 'sanjana'
+print(name[1:-1])
+
+
+# In[ ]:
+
+
+
+
