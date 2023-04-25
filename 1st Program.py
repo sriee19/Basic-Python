23172b8f121256f7ec747b6acfbf25c3f10726d9
SrisanjanaA <88726874+sriee19@users.noreply.github.com>
2023-04-25 23:36:32 +0530

    Add files via upload

---
+++ 1st Program.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# My first Python Program
+
+# In[1]:
+
+
+print("Hello")
+
+
+# Imaginary Dog
+
+# In[4]:
+
+
+print('0----')
+print(' ||||')
+
+
+# Multiplication Operator
+
+# In[5]:
+
+
+print('*' * 10)
+
+
+# In[ ]:
+
+
+
+
