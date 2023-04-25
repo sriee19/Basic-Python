601b34fd5964529f5debcc148b8db36f0971fc23
SrisanjanaA <88726874+sriee19@users.noreply.github.com>
2023-04-25 23:47:03 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Variables
+
+# The variables are assigned to the values; and could be used for printing.
+
+# In[1]:
+
+
+price = 10 
+print (price)
+
+
+# In[3]:
+
+
+price = 21
+price
+
+
+# In[5]:
+
+
+rating = 3.2
+print(rating)
+
+
+# In[6]:
+
+
+name = 'san'
+is_published = False
+print(name)
+
+
+# We check in a patient named john smith.
+# He is 20yrs old and is a new patient.
+
+# In[7]:
+
+
+name ='john smith'
+age = 20
+is_new = True
+print (name , age )
+
+
+# In[ ]:
+
+
+
+
