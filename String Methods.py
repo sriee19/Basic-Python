03b6cfd2fc1b5b8bdf617836f6a3427d9d114ae5
SrisanjanaA <88726874+sriee19@users.noreply.github.com>
2023-04-30 21:57:13 +0530

    Add files via upload

---
+++ String Methods.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # String Methods
+# 
+
+# In[1]:
+
+
+course = 'Python for Beginners'
+print(len(course))
+
+
+# methods
+# 
+
+# In[3]:
+
+
+print(course.upper())
+
+
+# In[4]:
+
+
+course
+
+
+# In[5]:
+
+
+print(course.lower())
+
+
+# to find the character
+
+# In[9]:
+
+
+course.find('n')
+
+
+# In[10]:
+
+
+print(course.replace('Beginners' , 'Absolute Beginners'))
+
+
+# To check the word in the sentence
+# 
+
+# In[12]:
+
+
+print('python' in course)
+
+
+# In[ ]:
+
+
+
+
