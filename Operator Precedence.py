3cb32f40b0bafa8683813b2cc7897bebced84634
SrisanjanaA <88726874+sriee19@users.noreply.github.com>
2023-05-01 17:53:14 +0530

    Add files via upload

---
+++ Operator Precedence.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Operator Precedence
+
+# every operators have preference
+
+# In[2]:
+
+
+x = 10+3*2
+x
+
+
+# here multi opertaor is given higher preference
+
+# # Order of opertions are
+
+# exponentiation 2**3 ////
+# multiplication or division////
+# addition or subtraction
+
+# In[3]:
+
+
+x = 10 + 3 * 2 ** 2
+
+
+# In[4]:
+
+
+x
+
+
+# In[5]:
+
+
+## parenthesis always takes the first priority
+
+
+# In[6]:
+
+
+x = ( 2 + 3 ) * 10 - 3
+## the answer would be = 47
+x
+
+
+# In[ ]:
+
+
+
+
