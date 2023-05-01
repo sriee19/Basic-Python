29e11ea90bed114146eb8f09dded4f151f415064
SrisanjanaA <88726874+sriee19@users.noreply.github.com>
2023-05-01 17:45:10 +0530

    Add files via upload

---
+++ Arithmetic Operations.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Arithmetic Operations
+
+# In[1]:
+
+
+print(10+ 3)
+
+
+# In[2]:
+
+
+print(10 - 3)
+
+
+# In[3]:
+
+
+print(10 * 3)
+
+
+# In[4]:
+
+
+print(10 / 3)
+
+
+# In[5]:
+
+
+print(10 // 3)
+
+
+# In[6]:
+
+
+print(10 % 3)
+
+
+# In[7]:
+
+
+print(10 ** 3)
+
+
+# In[8]:
+
+
+x = 10
+x = x+3
+print(x)
+
+
+# In[9]:
+
+
+x+=13
+x
+
+
+# In[ ]:
+
+
+
+
