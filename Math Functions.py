b3767d06209abb82638d3561c262f8d81169ff7e
SrisanjanaA <88726874+sriee19@users.noreply.github.com>
2023-05-01 18:02:59 +0530

    Add files via upload

---
+++ Math Functions.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Math Functions
+
+# In[1]:
+
+
+X = 2.9
+round(X)
+
+
+# In[2]:
+
+
+abs(-2.9)
+print(abs)
+
+
+# In[3]:
+
+
+print(abs(-2.9))
+
+
+# In[4]:
+
+
+import math
+
+
+# In[5]:
+
+
+print(math.ceil(2.9))
+
+
+# In[6]:
+
+
+print(math.floor(2.9))
+
+
+# Open up the browser and search python 3 math module
+
+# Refer the documents for all the math modules and functions 
+
+# In[ ]:
+
+
+
+
